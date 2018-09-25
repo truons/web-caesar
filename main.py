fca24741e279ec5f6a8f2355fdf2d12c3e074e1c
truons <42111116+truons@users.noreply.github.com>
2018-09-24 20:34:09 -0500

    working on web caesar

--- main.py
+++ main.py
@@ -1,26 +1,56 @@
 from flask import Flask, request
 from caesar import rotate_string
-import os
-import jinja2
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader )
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-add_form = """
- <form action="/add" method="POST">
-    <label>
-        Rotate by:
-        <input type="text" name="rot"/>
-    </label>
-    <input type="Submit" name="text"/>
-    </form>
-"""   
-@app.route("/add", methods=['POST'])
-<h1></h1>
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+           }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+        </style>
+    </head>
+    <body>
+        <form action="/",  method="post">
+            <label>
+                Rotate by:
+                <input type="text" name="rot" value="0">
+            </label><br>
+            <textarea name="text" cols="70" rows="10">{0}</textarea><br>
+            <input type="submit" value="Submit Query" name="submit">
+        </form>
+    </body>
+</html>
+
+ 
+"""
+@app.route("/")   
 def index():
-    return form.format(...)
+    return form.format("")
+   
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = int(request.form['rot'])
+    message = request.form['text']
+    text = rotate_string(message, rot)
+    return form.format(text)
+    
+
 
 app.run()    
